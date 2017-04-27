9dc53af00727ccf8cf07b091ac7effe2f685529f
Roberto Tomás Collins <roberto.tomas.cuentas@gmail.com>
2017-04-27 15:10:41 -0400

    .

--- config.js
+++ config.js
@@ -3,17 +3,16 @@ System.config({
   map: {
     'react': '//unpkg.com/react@15.5.0/dist/react.js',
     'react-dom': '//unpkg.com/react-dom@15.5.0/dist/react-dom.js',
-    'react-redux': '//wzrd.in/standalone/react-redux@latest',
     'redux': '//unpkg.com/redux@3.6.0/dist/redux.js',
+    'react-redux': '//wzrd.in/standalone/react-redux@latest',
 //    'chai': '//unpkg.com/chai@3.5.0',
 //    'mocha': '//unpkg.com/mocha@3.3.0'
 
     'app': './app'
   },
-  depCache: {
-    'react-redux': ['react', 'redux'],
-    'react-dom': ['react'] 
-  },
+  path: {
+    'react-redux': '//wzrd.in/standalone/react-redux@latest',
+  }
   meta: {
     'react-redux': {
       deps: [ 'react', 'redux']
