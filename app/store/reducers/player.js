0d822447adbe58ea340fab799362e2472added8b
Roberto Tomás Collins <roberto.tomas.cuentas@gmail.com>
2017-04-27 18:36:02 -0400

    state=0

--- app/store/reducers/player.js
+++ app/store/reducers/player.js
@@ -3,12 +3,12 @@ import Player from '../../objects/Player.js'
 let player = undefined
 let name = 'O Patife'
 
-export default (state, action) => {
+export default (state=0, action) => {
   if(action.reducer !== 'player')
     return state
   switch (action.type) {
     case 'NEW':
-      return player = new Player(Object.assign({}, {name}, action.options))
+      return player = new Player(Object.assign({name}, action.options))
       break
     case 'SET NAME':
       return name = action.name
