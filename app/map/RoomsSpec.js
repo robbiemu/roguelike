109d7889375fc45d85a9d10bb40a26e0918e7287
Roberto Tomás Collins <roberto.tomas.cuentas@gmail.com>
2017-04-29 19:13:54 -0400

    v1..1.1 slightly rarer boss

--- app/map/RoomsSpec.js
+++ app/map/RoomsSpec.js
@@ -3,10 +3,10 @@ export default {
     Empty: 0.35,
     Up:  0.1,
     Down: 0.1,
-    Boss: 0.05,
+    Boss: 0.025,
     Spawner: 0.1,
     Monster: 0.2,
-    Treasure: 0.1
+    Treasure: 0.125
   },
   required: ['Up','Down'],
   defaultType: 'Empty',
