67b8eeb0467c2fd3f169c20bb52033db5b636ed4
Roberto Tomás Collins <roberto.tomas.cuentas@gmail.com>
2017-05-01 21:14:22 -0400

    v.1.2.7
    fixed bug with moveAwayFrom .. creatures should now run away (At elast a bit) when injured!

--- app/GameEngine.js
+++ app/GameEngine.js
@@ -121,7 +121,7 @@ export default class GameEngine {
       x:-1 * (fromCreature.position.x - creature.position.x),
       y:-1 * (fromCreature.position.y - creature.position.y)
     }
-    moveTowardDest (creature, dest)
+    this.moveTowardDest (creature, dest)
   }
 
   plotPath (creature, dest) {
